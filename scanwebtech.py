784ae97f2778e3b7f97fa20c58f280c7b97f1549
Surgat <53380435+surgatengit@users.noreply.github.com>
2025-01-08 01:24:01 +0100

    Create scanwebtech.py

---
+++ scanwebtech.py
@@ -0,0 +1,17 @@
+import json
+import requests
+from webappanalyzer.webappanalyzer import WebAppAnalyzer
+from webappanalyzer.web_page import WebPage
+import sys
+
+def analyze_website(url):
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0"})
+    page = WebPage.new_from_response(response)
+    print(json.dumps(WebAppAnalyzer().analyze(page), indent=2))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python scanwebtechs.py <url>")
+        sys.exit(1)
+    url = sys.argv[1]
+    analyze_website(url)
